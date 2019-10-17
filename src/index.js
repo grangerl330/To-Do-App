10fddff870b14267bb27d7b860a03a85e6f82d4d
grangerl330 <grangerl330@gmail.com>
2019-10-16 22:59:19 -0400

    set up the Redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Importing Provider for Redux
+import { Provider } from 'react-redux'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Importing the store to be passed into Provider
+import store from './store.js'
+
+ReactDOM.render(
+  // Wrapped <App /> component in <Provider> with prop of store passed in to allow access to the store within the App
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/store.js
@@ -0,0 +1,19 @@
+// Imports necessary for Redux
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+// Redux Thunk is a middleware that lets you call action creators that return a function instead of an action object.
+import thunk from 'redux-thunk'
+
+// Combining all reducer files into one to be passed in to the createStore function
+const reducer = combineReducers({
+
+})
+
+// Added composeEnhancer which allows us to use redux dev tools in console
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Creating the store
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+// Export the store to be imported by the App
+export default store
