56de3bd5c9182dd519ea50a00e01cd3bfdb98338
grangerl330 <grangerl330@gmail.com>
2019-10-20 19:12:06 -0400

    added actions for currentUser and connect to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// Imports action to fetch the currentUser from the database
+import { getCurrentUser } from './actions/currentUser'
+// Imports connect function which allows for connection to the Redux store
+import { connect } from 'react-redux'
+// Imports css file for App
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>To-Do App</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    // Calls the getCurrentUser() function when the component initially mounts when page loads
+    this.props.getCurrentUser()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To-Do App</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
---
+++ src/actions/currentUser.js
@@ -0,0 +1,55 @@
+// Synchronous action creators
+
+// Accepts argument, user, which is json data of user retrieved from database
+export const setCurrentUser = user => {
+  // Returns action object - The type will be recognized by the reducer and the user data will be added to the store
+  return {
+    type: 'SET_CURRENT_USER',
+    user
+  }
+}
+
+// Asynchronous action creators
+
+export const login = credentials => {
+  return dispatch => {
+    return fetch('/login', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (user.error) {
+        alert(user.error)
+      } else {
+        dispatch(setCurrentUser(user))
+      }
+    })
+    .catch(console.log)
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch('/get_current_user', {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (user.error) {
+        console.log(user.error)
+      } else {
+        dispatch(setCurrentUser(user))
+      }
+    })
+    .catch(console.log)
+  }
+}
