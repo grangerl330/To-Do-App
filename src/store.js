67ba28dd721e69badceb6a62e25a1c0124a654f5
grangerl330 <grangerl330@gmail.com>
2019-10-22 23:07:23 -0400

    added currentUser reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 // Imports necessary for Redux
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
+// Reducer File Imports
+import currentUser from './reducers/currentUser.js'
+
 // Redux Thunk is a middleware that lets you call action creators that return a function instead of an action object.
 import thunk from 'redux-thunk'
 
 // Combining all reducer files into one to be passed in to the createStore function
 const reducer = combineReducers({
-
+  currentUser
 })
 
 // Added composeEnhancer which allows us to use redux dev tools in console
