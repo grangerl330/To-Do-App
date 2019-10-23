ff7da705055bfd1fafddff224db5a21475ee049c
grangerl330 <grangerl330@gmail.com>
2019-10-22 23:20:29 -0400

    added explanation comments to currentUser reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,14 @@
+// Recieves action object from action creator when action creator is called in componentDidMount() in App.js
 export default (state = null, action) => {
+  // Checks type property of action object
   switch(action.type) {
+    // Returns the user property of the action object, which is the json of the user from the database. This sets the currentUser portion of the Redux store to this data
     case 'SET_CURRENT_USER':
       return action.user
+    // Returns the default state, which sets the currentUser portion of the store to null, effectively removing the currentUser data
     case 'CLEAR_CURRENT_USER':
       return null
+    // If the type property does not match anything above, sets to currentUser portion of the store to whatever it currently is, which keeps the data the same
     default:
       return state
   }
