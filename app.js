86cb4cb589b861ad9560dc4310659ed5d513116b
grangerl330 <grangerl330@gmail.com>
2019-12-03 14:17:20 -0500

    defined UI variables in app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Define UI Variables
+const form = document.querySelector('#to-do-form')
+const toDoList = document.querySelector('#to-do-collection')
+const clearButton = document.querySelector('#clear-to-dos')
+const filter = document.querySelector('#filter')
+const toDoInput = document.querySelector('#to-do')
+
